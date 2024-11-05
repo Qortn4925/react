ef4166d9d54341c5702247b38bb6e2f48cea2d8d
qortn4925 <qortn4925@naver.com>
2024-11-05 16:44:32 +0900

    컴포넌트 야미

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import MyApp2 from "./apps/MyApp2.jsx";
+import App2 from "./pratice/App2.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MyApp2 />
+    <App2 />
   </StrictMode>,
 );
---
+++ src/pratice/App2.jsx
@@ -0,0 +1,18 @@
+function MyComp() {
+  return (
+    <div>
+      <h1>첫 번째 컴포넌트</h1>
+      <h1>첫 번째 컴포넌트</h1>
+    </div>
+  );
+}
+function App2() {
+  return (
+    <div>
+      <MyComp />
+      <MyComp />
+    </div>
+  );
+}
+
+export default App2;
