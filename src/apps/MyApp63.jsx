febcdad83250f7a8c5a36ffad68cd82e2ad4a239
qortn4925 <qortn4925@naver.com>
2024-11-12 10:46:05 +0900

    javascript , get,post,delete,put 다양한 방식의 request

--- src/apps/MyApp63.jsx
+++ src/apps/MyApp63.jsx
@@ -12,6 +12,52 @@ function MyApp63(props) {
       >
         get1
       </Button>
+
+      <Button
+        onClick={() => {
+          axios.post("/api/main3/sub2");
+        }}
+      >
+        post
+      </Button>
+
+      <Button
+        onClick={() => {
+          axios.put("/api/main3/sub3");
+        }}
+      >
+        put
+      </Button>
+
+      <Button
+        onClick={() => {
+          axios.put("/api/main3/sub4", { title: "제목", author: "저자" });
+        }}
+      >
+        put , 데이터
+      </Button>
+
+      <Button
+        onClick={() => {
+          axios.delete("/api/main3/sub5");
+        }}
+      >
+        delete
+      </Button>
+
+      <Button
+        onClick={() => {
+          axios.delete("/api/main3/sub6", {
+            data: {
+              id: 5,
+              name: "kim",
+            },
+          });
+        }}
+      >
+        {" "}
+        delete 데이터
+      </Button>
     </div>
   );
 }
